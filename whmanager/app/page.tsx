b67c73c96431950aa2d85ba4d21bb844681be2bf
KlenisT <159272283+KlenisT@users.noreply.github.com>
2025-10-04 17:40:07 +0300

    delete page under dashboard // vercel error

--- whmanager/app/page.tsx
+++ whmanager/app/page.tsx
@@ -9,7 +9,7 @@ export default function HomePage() {
       <div className={styles.mainContent}>
         <Header />
         <main className={styles.content}>
-          <h1>Welcome to WH Manager // Ruudi</h1>
+          <h1>Welcome to WH Manager //</h1>
           <p>This is your dashboard overview.</p>
         </main>
       </div>
